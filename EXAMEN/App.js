c41a3b9fad475766757b948aae4a53a9ce16ece2
KevinxDx3 <85362446+KevinxDx3@users.noreply.github.com>
2023-10-02 09:23:22 -0500

    Navegacion

--- EXAMEN/App.js
+++ EXAMEN/App.js
@@ -1,12 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+//importar pantallas
+import {Autos} from './app/screen/Autos';
+import {Clientes} from './app/screen/Clientes';
+import { Loguin } from './app/screen/Loguin';
+import { Reservas } from './app/screen/Reservas';
+import { Menu } from './app/screen/Menu';
+
+
+
+const Stack = createStackNavigator();
 
 export default function App() {
+
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={Loguin} />
+        <Stack.Screen name="Clientes" component={Clientes} />
+        <Stack.Screen name="Autos" component={Autos} />
+        <Stack.Screen name="Reservas" component={Reservas} />
+        <Stack.Screen name="Menu" component={Menu}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+
   );
 }
 
---
+++ EXAMEN/app/screen/Menu.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+export const Menu = () => {
+
+    
+
+}
